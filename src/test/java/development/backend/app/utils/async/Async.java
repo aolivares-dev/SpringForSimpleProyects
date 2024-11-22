29c2eb2a5d9ce6f41451a471c3a47b58123b6fb7
Alessandro Olivares <alessandro.olivares@utp.ac.pa>
2024-11-21 22:59:48 -0500

    pa produccion

--- src/test/java/development/backend/app/utils/async/Async.java
+++ src/test/java/development/backend/app/utils/async/Async.java
@@ -1,4 +1,13 @@
 package development.backend.app.utils.async;
 
+import org.junit.jupiter.api.Test;
+
 public class Async {
+	@Test
+	void run() {
+		Runnable runnable = () -> {
+			System.out.println("Hello from thread: " + Thread.currentThread().getName());
+		};
+		new Thread(runnable).start();
+	}
 }
